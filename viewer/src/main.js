0e5cf8f3d76844c4a8095f89d9e95c1486b54859
NieneB <niene.boeijen@gmail.com>
2018-05-02 15:54:40 +0200

    viewer for app

--- viewer/src/main.js
+++ viewer/src/main.js
@@ -1,12 +1,15 @@
 var mapboxgl = require('mapbox-gl');
 var d3 = require('d3');
+var typeahead = require('typeahead.js');
+var Bloodhound = require('bloodhound-js');
+var $ = require('jquery');
 
 // MAP OPTIONS
 var options = {
   container: "map",
   hash: true,
   style:'./style/style.json',
-  zoom: 10,
+  zoom: 15,
   pitch: 0,
   bearing: 0,
   center: [5.16199, 52.16],
@@ -85,3 +88,99 @@ map.on('click', 'perceel', function (e) {
     .setHTML("waarde:  "+ description + "</br>" + "perceel nummer:  "+ perceel + "</br>" + "sectie:  " + sectie)
     .addTo(map);
 });
+
+
+
+// SEARCH BAR
+var searchdata = {};
+function enableTypeahead() {
+  $('#ttinput .typeahead').typeahead({
+    hint: false,
+    highlight: true,
+    minLength: 2
+  },
+    {
+      name: 'PDOK',
+      displayKey: 'value',
+      display: 'value',
+      limit: 5,
+      source: searchdata.bloodhoundengine,
+      templates: {
+        empty: [
+          "<div class='noitems'>",
+          'Niets gevonden',
+          '</div>'
+        ].join('\n')
+      }
+    });
+};
+
+var searchInputListeners = function () {
+  $('#ttinput .typeahead').on('typeahead:select', function (ev, suggestion) {
+    getCoordinates(suggestion);
+  });
+  $('#ttinput .typeahead').on('typeahead:autocomplete', function (ev, suggestion) {
+    getCoordinates(suggestion);
+  });
+};
+// SUGGESTION API
+function enableBloodhound() {
+  searchdata.bloodhoundengine = new Bloodhound({
+    datumTokenizer: function (d) { return Bloodhound.tokenizers.whitespace(d.value); },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      url: 'https://geodata.nationaalgeoregister.nl/locatieserver/suggest?rows=10&fq=type:woonplaats&q=%QUERY',
+      wildcard: '%QUERY',
+      transform: function (response) {
+        return $.map(response.response.docs, function (item) {
+          return {
+            value: item.weergavenaam,
+            id: item.id
+          };
+        });
+      }
+    }
+  });
+  searchdata.bloodhoundengine.initialize();
+};
+
+// GET COORDINATES OF CHOSEN LOCATION API
+function getCoordinates(suggestion) {
+  var plaats = suggestion.id;
+  var url = 'https://geodata.nationaalgeoregister.nl/locatieserver/lookup?wt=json&id=' + plaats;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var myArr = JSON.parse(this.responseText);
+      getTarget(myArr);
+    }
+  };
+  xmlhttp.open('GET', url, true);
+  xmlhttp.send();
+};
+
+// GET TARGET
+function getTarget(plaats) {
+  var target = [];
+  var locatieString = plaats.response.docs[0].centroide_ll;
+  target.push(Number(locatieString.substring(6, 16)), Number(locatieString.substring(17, locatieString.length - 1)));
+  recenterMap(target);
+};
+
+// RECENTER MAP ON CHOSEN LOCATION
+function recenterMap(target) {
+  map.jumpTo({
+    center: target,
+    zoom: 15.5,
+    speed: 1,
+    minZoom: 15
+  });
+};
+
+// START SEARCH
+function initSearch() {
+  enableBloodhound();
+  enableTypeahead();
+  searchInputListeners();
+};
+initSearch();
